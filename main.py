ac24c990bb7b8aa125fad3bf7d20f6e6f514ef6c
PrashilLamichhane <prashillamichhane123@gmail.com>
2026-02-14 21:59:00 +0545

    Request Body

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get('/')
+@app.get('/blog')
 def index():
     return {'data' : 'blog list'}   
 
@@ -14,7 +16,7 @@ def unpublished():
 
 
 
-@app.get('/blog/{id}')
+@app.get('/blog/{id}')   
 def about(id: int):
     return {'data' : id}
 
@@ -24,3 +26,13 @@ def about(id: int):
 @app.get('/blog/{id}/comments')
 def comment(id: int):
          return {'data' : f'blog {id} comments'}
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+
+@app.post('/blog')
+def create_blog(request: Blog):
+   
+    return {'data' : f'blog is created with title as {request.title}'}
