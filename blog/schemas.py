8f51912da9ed62381e390d5d799c9e23a5126cc4
PrashilLamichhane <prashillamichhane123@gmail.com>
2026-02-18 16:49:02 +0545

    Response Model

--- blog/schemas.py
+++ blog/schemas.py
@@ -1,5 +1,15 @@
 from pydantic import BaseModel
+# only for pydantic model we call it a schemas 
 
 class Blog(BaseModel):
     title: str
     body: str
+    
+
+
+
+class ShowBlog(BaseModel):
+    title : str
+    class Config():
+        orm_mode = True
+    
