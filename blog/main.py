6b87228de1ffeb3b4d1dddc862f00e68350d5880
PrashilLamichhane <prashillamichhane123@gmail.com>
2026-02-18 14:05:33 +0545

    add items to table

--- blog/main.py
+++ blog/main.py
@@ -1,11 +1,24 @@
-from fastapi import FastAPI
-from . import schemas
+from fastapi import FastAPI, Depends
+from . import schemas,models
+from .database import engine, SessionLocal
+from sqlalchemy.orm import Session
+
+
 app = FastAPI()
 
 
+models.Base.metadata.create_all(bind = engine)
 
-
-
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 @app.post("/")
-def create(request: schemas.Blog):
-    return {"data": f"blog created with title '{request.title}' and body '{request.body}'"}
+def create(request: schemas.Blog, db : Session = Depends(get_db)):
+    new_blog = models.Blog(title = request.title, body = request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
