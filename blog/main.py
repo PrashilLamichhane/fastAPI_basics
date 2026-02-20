c9ea8f0ab92283aead8b1d7bc68db2682834b814
PrashilLamichhane <prashillamichhane123@gmail.com>
2026-02-20 23:59:01 +0545

    Tags added

--- blog/main.py
+++ blog/main.py
@@ -16,7 +16,7 @@ def get_db():
     finally:
         db.close()
         
-@app.post("/blog",status_code = status.HTTP_201_CREATED)
+@app.post("/blog",status_code = status.HTTP_201_CREATED,tags=['blog'])
 def create(request: schemas.Blog, db : Session = Depends(get_db)):
     new_blog = models.Blog(title = request.title, body = request.body)
     db.add(new_blog)
@@ -24,14 +24,14 @@ def create(request: schemas.Blog, db : Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
-@app.get("/blog", response_model = list[schemas.ShowBlog])
+@app.get("/blog", response_model = list[schemas.ShowBlog], tags=['blog'])
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
 
 
     return blogs
     
-@app.get("/blog/{id}",response_model  = schemas.ShowBlog)
+@app.get("/blog/{id}",response_model  = schemas.ShowBlog, tags=['blog'])
 def show(id:int,  db:Session = Depends(get_db)):
 
     blog = db.query(models.Blog).filter(models.Blog.id == id).first() 
@@ -41,7 +41,7 @@ def show(id:int,  db:Session = Depends(get_db)):
     return blog
 
 
-@app.post('/user')
+@app.post('/user', tags=['user'])
 def create_user(request : schemas.User, db:Session = Depends(get_db)):
     new_user = models.User(name = request.name, email = request.email, password = Hash.bcrypt(request.password))
     db.add(new_user)
@@ -49,7 +49,7 @@ def create_user(request : schemas.User, db:Session = Depends(get_db)):
     db.refresh(new_user)
     return new_user
 
-@app.delete('/user/{id}', status_code=status.HTTP_204_NO_CONTENT)
+@app.delete('/user/{id}', status_code=status.HTTP_204_NO_CONTENT, tags = ['user'])
 def delete_user(id: int, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == id).first()
     
@@ -60,7 +60,7 @@ def delete_user(id: int, db: Session = Depends(get_db)):
     db.commit()
     return {'detail': 'User deleted'}
 
-@app.get('/user/{id}', response_model=schemas.ShowUser)
+@app.get('/user/{id}', response_model=schemas.ShowUser, tags=['user'])
 def get_user(id: int, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == id).first()
     if not user:
